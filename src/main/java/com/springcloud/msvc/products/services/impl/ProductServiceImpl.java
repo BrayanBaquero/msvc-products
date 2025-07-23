e1afc9d5f691f313f1d7a99642f1709692cf748c
BrayanBaquero <73759467+BrayanBaquero@users.noreply.github.com>
2025-07-22 21:50:53 -0500

    Add load balancing configuration

--- src/main/java/com/springcloud/msvc/products/services/impl/ProductServiceImpl.java
+++ src/main/java/com/springcloud/msvc/products/services/impl/ProductServiceImpl.java
@@ -4,11 +4,13 @@ import com.springcloud.msvc.products.entities.Product;
 import com.springcloud.msvc.products.repositories.ProductRpository;
 import com.springcloud.msvc.products.services.ProductService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -16,16 +18,24 @@ public class ProductServiceImpl implements ProductService {
 
 
     final private ProductRpository productRpository;
+    final private Environment environment;
 
     @Override
     @Transactional(readOnly = true)
     public List<Product> findAll() {
-        return (List<Product>) productRpository.findAll();
+        return ((List<Product>) productRpository.findAll()).stream()
+                    .map(product -> {
+                        product.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+                        return product;
+                }).collect(Collectors.toList());
     }
 
     @Override
     @Transactional(readOnly = true)
     public Optional<Product> findById(Long id) {
-        return productRpository.findById(id);
+        return productRpository.findById(id).map(product -> {
+            product.setPort(Integer.parseInt(environment.getProperty("local.server.port")));
+            return product;
+        });
     }
 }
