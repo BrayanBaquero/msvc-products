769ec58098454619192dc5688c2cb836d42c4276
BrayanBaquero <73759467+BrayanBaquero@users.noreply.github.com>
2025-07-31 22:29:16 -0500

    Add timeout example

--- src/main/java/com/springcloud/msvc/products/controllers/ProductController.java
+++ src/main/java/com/springcloud/msvc/products/controllers/ProductController.java
@@ -13,13 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @AllArgsConstructor
 @RequestMapping
 public class ProductController {
 
-    final private ProductService productService;
+     private final ProductService productService;
 
     @GetMapping
     public List<Product> list(){
@@ -27,8 +28,16 @@ public class ProductController {
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<Product> details(@PathVariable Long id){
+    public ResponseEntity<Product> details(@PathVariable Long id) throws InterruptedException {
         Optional<Product> product = productService.findById(id);
+        if (id.equals(10)){
+            throw new IllegalStateException("Producto no encontrado");
+        }
+        if (id.equals(7L)){
+            TimeUnit.SECONDS.sleep(2L);
+           // throw new IllegalStateException("Producto no encontrado");
+        }
+
         if (product.isPresent()){
             return ResponseEntity.ok(product.orElseThrow());
         }
